9a703b89c039eafa54136b8879b7a000c660d8a0
Eugene Derevyanko <derevyanko.eugene@gmail.com>
2019-09-06 22:38:41 +0300

    implement cached matrix object for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here is an implementation of cahing of inverted matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a cache object which
+## stores the matrix and its inversion and returns these matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv_arg) inv <<- inv_arg
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function expects a cached object returned by the makeCacheMatrix function,
+## tries to get cached value and if it does not exists,
+## inverts the matrix and puts it into the cache to get values from cache for subsequent calls
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
